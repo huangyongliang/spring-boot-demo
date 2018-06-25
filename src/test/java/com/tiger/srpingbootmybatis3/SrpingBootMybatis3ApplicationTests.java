7a1fe6d25cd3a73ce49e1cb6b954bcf3cb72fd78
huangyongliang <huangyongliang16@qq.com>
2018-06-26 02:56:22 +0800

    初始化项目

---
+++ src/test/java/com/tiger/srpingbootmybatis3/SrpingBootMybatis3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tiger.srpingbootmybatis3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SrpingBootMybatis3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
